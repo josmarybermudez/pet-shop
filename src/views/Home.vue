a33eef21bafc78a0b7875232c5bbe25b71307fbc
Josmary Bermudez <yokijb18@gmail.com>
2021-09-13 14:03:28 -0300

    First Commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1> {{ title }} </h1>
+    <p> {{ greeting() }} </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      title: 'Holis Jos'
+    }
   },
-};
+  methods: {
+    greeting: function(){
+      return 'heyyy, i didnt broke';
+    }
+  }
+}
 </script>
